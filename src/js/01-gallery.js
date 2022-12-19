2cbf2828a70c01fcf8f6aa741edf8c8e9c138a49
Max Semena <me.semena@gmail.com>
2022-12-19 18:02:49 +0200

    Added simplelightbox + 01-gallery files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemMarkup(galleryItems);
+populateGallery(galleryItemsMarkup);
+
+const gallery = new SimpleLightbox('.gallery a', { 
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+
+function createGalleryItemMarkup(galleryItems) {
+    return galleryItems.map(item => 
+       `<a class="gallery__item" href="${item.original}">
+            <img 
+                class="gallery__image" 
+                src="${item.preview}" 
+                alt="${item.description}" 
+            />
+        </a>`
+    ).join('');
+}
+
+
+function populateGallery(markup) {
+    galleryContainerEl.innerHTML = markup;
+}
