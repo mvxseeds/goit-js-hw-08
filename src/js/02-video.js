b7fbd7b1978cf4890c62af1c81e18edd16b93b35
Max Semena <me.semena@gmail.com>
2022-12-20 16:06:17 +0200

    Added 02 + 03 tasks solution source codes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_PLAYTIME_KEY = 'videoplayer-current-time';
+
+
+const onPlay = function(data) {
+    const savedPlaytime = localStorage.getItem(STORAGE_PLAYTIME_KEY);
+    
+    if (savedPlaytime) {
+        player.setCurrentTime(savedPlaytime);
+    }
+};
+
+const onTimeUpdate = function(data) {
+    let currentPlaytime = player.getCurrentTime().then(function(seconds) {
+        // seconds = the current playback position
+        localStorage.setItem(STORAGE_PLAYTIME_KEY, seconds);
+    }).catch(function(error) {
+        console.log("Error getting video player playtime value!")
+    });
+}
+
+player.on('play', onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]'),
+};
+
+const STORAGE_INPUT_KEY = 'feedback-form-state';
+
+
+refs.form.addEventListener('input', throttle(onFormFieldInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateFormInputs();
+
+
+function onFormFieldInput(e) {
+    const inputData = {
+        [e.target.name]: e.target.value
+    };
+
+    const savedInput = retrieveInputValues();
+    
+    if (!savedInput) {
+        saveInputValues(inputData);
+    } else {
+        const overrideInput = { ...savedInput, ...inputData };
+        saveInputValues(overrideInput);
+    }
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_INPUT_KEY);
+}
+
+function retrieveInputValues() {
+    return JSON.parse(localStorage.getItem(STORAGE_INPUT_KEY));
+}
+
+function saveInputValues(data) {
+    const dataJSON = JSON.stringify(data);
+    localStorage.setItem(STORAGE_INPUT_KEY, dataJSON);
+}
+
+function populateFormInputs() {
+    const savedInput = retrieveInputValues();
+
+    if (savedInput) {
+        refs.email.value = savedInput.email;
+        refs.message.value = savedInput.message;
+    }
+}
